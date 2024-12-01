e5e1a080a539553ef44e0460fe4da18f31b538fa
grabarv <grabar.vladyslav.nl@gmail.com>
2024-12-01 16:44:55 +0100

    make project structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
+    int b = 3;
+    int p = 10000;
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+
+    private void findMatches(String text, String pattern) {
+
+    }
+
+    private int hashFunction(String input) {
+        return 0;
+    }
+    private int rollingHashFunction(String input, int previousHash) {
+        return 0;
     }
 }
