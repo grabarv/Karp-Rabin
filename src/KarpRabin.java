62ef28f61547bb043be1da173506035a1aabbd63
grabarv <grabar.vladyslav.nl@gmail.com>
2024-12-01 17:22:04 +0100

    finish rollingHashFunction() method

--- src/KarpRabin.java
+++ src/KarpRabin.java
@@ -17,9 +17,9 @@ public class KarpRabin {
         int hash = -1;
         for (int i = 0; i < text.length() - pattern.length(); i++) {
             if (i == 0) {
-                hash = hashFunction(text.substring(i, i+pattern.length()));
+                hash = hashFunction(text.substring(i, i + pattern.length()));
             } else {
-                hash = rollingHashFunction(text.substring(i, i+pattern.length()), hash);
+                hash = rollingHashFunction(hash, text.charAt(i-1), text.charAt(i + pattern.length()));
             }
             if (hash == patternHash) {
                 if(text.substring(i, i+pattern.length()).equals(pattern)) {
@@ -33,11 +33,17 @@ public class KarpRabin {
     private int hashFunction(String input) {
         int hash = 0;
         for (int i = 0; i < input.length(); i++) {
-            hash += (int) (((int) input.charAt(i)) * Math.pow(b, input.length()-i-1));
+            hash += (int) (input.charAt(i) * Math.pow(b, pattern.length()-i-1) % p);
         }
         return hash;
     }
-    private int rollingHashFunction(String input, int previousHash) {
-        return 0;
+    private int rollingHashFunction(int previousHash, char firstCharOfPreviousString, char lastCharOfNewString) {
+        int hash = previousHash;
+
+        hash -= (int) (firstCharOfPreviousString * Math.pow(b, pattern.length() - 1));
+        hash *= 3;
+        hash += lastCharOfNewString;
+        hash %= p;
+        return hash;
     }
 }
